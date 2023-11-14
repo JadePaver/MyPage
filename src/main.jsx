7a782b4e297cf5b39eb08d5cef509a528cc4aa23
JadePaver <jade.paver@outlook.com>
2023-11-14 13:39:52 +0800

    final commit?

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,13 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import { Footer } from './components/Footer';
 import { Nav } from './components/Nav';
+import { Helmet } from 'react-helmet'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <Helmet>
+      <title>Paver Portfolio</title>
+    </Helmet>
     <Nav />
     <App />
     <Footer />
